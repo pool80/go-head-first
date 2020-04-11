fec1afd7cc7d8cfd074b6580093e4a6bec721045
Oleg Pometov <oleg@pool80.ru>
2020-04-11 16:12:09 +0300

    LF ending

---
+++ 03-methodsIntro/methods.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time" //импортируем пакет time
+)
+
+func main() {
+	//метод time.Now возвращает значение time.Time
+	var now time.Time = time.Now()
+
+	//у значений time.Time есть метод Year, который возвращает текущий год
+	var year int = now.Year()
+	fmt.Println(year)
+}
