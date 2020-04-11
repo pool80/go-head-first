de3c5c71f3b823c0835d9ef395935eaa6d7b0e97
Oleg Pometov <oleg@pool80.ru>
2020-04-11 14:36:13 +0300

    exercise with magnets about apple

---
+++ 02-exercise-var/magnet.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var originalCount int = 10
+	fmt.Println("I started with", originalCount, "apples.")
+
+	var eatenCount int = 4
+	fmt.Println("Some jerk ate", eatenCount, "apples.")
+	fmt.Println("There are", originalCount-eatenCount, "apples left")
+}
