30926af461c78cd8855af82c4907cc6e971251c5
Oleg Pometov <oleg@pool80.ru>
2020-04-14 16:04:33 +0300

    printf

--- 04-fuctions/01-doubleCode/doubleCode.go
+++ 04-fuctions/01-doubleCode/doubleCode.go
@@ -8,10 +8,10 @@ func main() {
 	width := 4.2
 	height := 3.0
 	area := width * height
-	fmt.Println(area/10, "litters needed")
+	fmt.Printf("%.2f litters needed\n", area/10)
 
 	width = 5.2
 	height = 3.5
 	area = width * height
-	fmt.Println(area/10, "litters needed")
+	fmt.Printf("%.2f litters needed\n", area/10)
 }
