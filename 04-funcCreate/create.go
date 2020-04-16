79ff456029bd63fbb9e2a911d8603a606326aa54
Oleg Pometov <oleg@pool80.ru>
2020-04-16 22:33:09 +0300

    create own functions

--- 04-funcCreate/create.go
+++ 04-funcCreate/create.go
@@ -1,20 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+var metersPerLiter float64
 
 //ширина и высота стены передаются параметрами
-func paintNeeded(width float64, height float64) {
+func paintNeeded(width, height float64) float64 {
 
 	//ширина умножается на высоту
 	area := width * height
 
+	//возвращаем значение переменной
+	return area / metersPerLiter
 	//выводим расход краски
-	fmt.Printf("%.2f liters needed\n", area/10.0)
+	//fmt.Printf("%.2f liters needed\n", area/10.0)
 }
 
 func main() {
-	paintNeeded(4.2, 3.0)
-	paintNeeded(77.235, 5.6)
+	metersPerLiter = 10
+	fmt.Printf("%.2f\n", paintNeeded(4.2, 3.0))
+	fmt.Printf("%.2f\n", paintNeeded(77.235, 5.6))
 }
