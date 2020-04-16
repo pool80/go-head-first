1054d78b59738667805eea4454352fe3c80a3c50
Oleg Pometov <oleg@pool80.ru>
2020-04-16 14:45:26 +0300

    create own functions

--- 04-funcCreate/create.go
+++ 04-funcCreate/create.go
@@ -1,1 +1,20 @@
-package _4_funcCreate
+package main
+
+import (
+	"fmt"
+)
+
+//ширина и высота стены передаются параметрами
+func paintNeeded(width float64, height float64) {
+
+	//ширина умножается на высоту
+	area := width * height
+
+	//выводим расход краски
+	fmt.Printf("%.2f liters needed\n", area/10.0)
+}
+
+func main() {
+	paintNeeded(4.2, 3.0)
+	paintNeeded(77.235, 5.6)
+}
