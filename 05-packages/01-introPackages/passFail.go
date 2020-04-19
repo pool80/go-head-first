2de17b8550769cd3499f06dcc8dbf0366a1bbce9
Oleg Pometov <oleg@pool80.ru>
2020-04-19 23:18:02 +0300

    разные прграммы одна функция

---
+++ 05-packages/01-introPackages/passFail.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		//если при чтении ввода произошла ошибка, она будет возвращена функцией
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
+
+func main() {
+	fmt.Print("Enter a grade:")
+	grade, err := getFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	//сравниваем с float64
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+
+	fmt.Println("A grade of", grade, "is", status)
+}
