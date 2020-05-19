b39ad4e34feb56dc4efb3e5c3ef337c70f061d2e
Oleg Pometov <oleg@pool80.ru>
2020-05-19 23:28:32 +0300

    пакеты

--- 05-packages/03-hiPackages/hiPackages.go
+++ 05-packages/03-hiPackages/hiPackages.go
@@ -1,10 +1,14 @@
 package main
 
 //импортируем созданный пакет
-import "greeting"
+import (
+	"greeting"
+	"greeting/deutsch"
+)
 
 func main() {
 	//при вызове пакета нужно указать имя пакета и точку
 	greeting.Hello()
 	greeting.Hi()
+	deutsch.GutenTag()
 }
